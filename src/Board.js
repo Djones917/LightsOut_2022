913448655157164a97fa4517aa408f326feb8e30
Derek <nightwing917@hotmail.com>
2022-10-11 20:16:15 -0800

    fixed typo in board push

--- src/Board.js
+++ src/Board.js
@@ -55,7 +55,7 @@ class Board extends Component {
       for(x=0; x < this.props.ncols; x++) {
         row.push(Math.random() < this.props.chanceLightStartsOn);
       }
-      board.row(row);
+      board.push(row);
     }
     return board
   }
