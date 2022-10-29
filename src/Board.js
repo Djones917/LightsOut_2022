55ad8e6171edcb0435accde88f13e6f93b60c797
Derek <nightwing917@hotmail.com>
2022-10-28 19:50:52 -0800

    working on flipcellsaroundme

--- src/Board.js
+++ src/Board.js
@@ -101,7 +101,7 @@ class Board extends Component {
       let row = [];
       for(let x = 0; x < this.props.ncols; x++) {
         let coord = `${y}-{x}`;
-        row.push(<Cell key={coord} isLit={this.state.board[y][x]}/>);
+        row.push(<Cell key={coord} isLit={this.state.board[y][x]} flipCellsAroundMe={this.flipCellsAround}/>);
       }
       tblBoard.push(<tr key={y}>{row}</tr>);
     }
